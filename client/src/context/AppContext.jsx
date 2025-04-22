a5633b314f3dae2ec69b8fec1bfdfda9bdba4c10
immobarok <immobarokali2001@gmail.com>
2025-04-23 00:44:23 +0600

    server added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,8 +5,9 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
   const [user, setUser] = React.useState(null);
   const [showLogin, setShowLogin] = React.useState(false);
+  const backendUrl = import.meta.env.BACKEND_URI;
   const value = {
-    user, setUser, showLogin, setShowLogin
+    user, setUser, showLogin, setShowLogin,backendUrl
   }
   return (
     <AppContext.Provider value={value}>
