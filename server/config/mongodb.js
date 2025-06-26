9692a441546ccdda1e8d854eb40d14002a73a98e
immobarok <immobarokali2001@gmail.com>
2025-06-26 18:13:31 +0600

    updated

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -3,6 +3,6 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("MongoDB connected successfully");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/neurovision`)
+  await mongoose.connect(`${process.env.MONGODB_URI}`)
 }
 export default connectDB;
